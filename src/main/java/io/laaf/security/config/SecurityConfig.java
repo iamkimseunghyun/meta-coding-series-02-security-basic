da91fc789384199a97fc44331fe357b4d398ef74
kaka <iamkimseunghyun@gmail.com>
2022-01-19 10:49:46 +0900

    스큐리티 특강 - 7강 완료 2022.01.19

--- src/main/java/io/laaf/security/config/SecurityConfig.java
+++ src/main/java/io/laaf/security/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package io.laaf.security.config;
 
+import io.laaf.security.config.oauth.PrincipalOauth2UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -12,6 +14,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @EnableWebSecurity // 스프링 시큐리티 필터가 스프링 필터 체인에 등록이 됩니다.
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true) // secure 어노테이션 활성, preAuthorize 활성화
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private PrincipalOauth2UserService principalOauth2UserService;
 
     // 해당 메서들의 리턴 오브젝트를 ioc로 등록해줌
     @Bean
@@ -35,6 +40,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .defaultSuccessUrl("/")
                 .and()
                 .oauth2Login()
-                .loginPage("/loginForm");
+                .loginPage("/loginForm")
+                .userInfoEndpoint()
+                .userService(principalOauth2UserService);
     }
 }
